c9e5a596c1785e3bd7c72fe1f80b5095fb3868ac
hossainmithu <hossainmithu248@gmail.com>
2024-09-25 15:17:51 +0600

    added history section

---
+++ js/features.js
@@ -0,0 +1,11 @@
+document.getElementById('btn-donation')
+  .addEventListener('click', function(){
+    console.log('donation btn clicked');
+    showSectionById('donation-section');
+  })
+
+  document.getElementById('btn-history')
+  .addEventListener('click', function(){
+    console.log('donation btn clicked');
+    showSectionById('history-section');
+  })
--- js/utilities.js
+++ js/utilities.js
@@ -15,3 +15,11 @@ function getTextFieldValueById(id){
   return balanceNumber;
 
 }
+
+
+function showSectionById(id){
+  document.getElementById('donation-section').classList.add('hidden');
+  document.getElementById('history-section').classList.add('hidden');
+
+  document.getElementById(id).classList.remove('hidden');
+}
